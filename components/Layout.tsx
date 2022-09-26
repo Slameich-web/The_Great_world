f32e7c967c5a9861ff99e08fcefc2636937ac969
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-09-26 13:10:57 +0300

    registation start

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,9 @@ import styles from '../styles/Layout.module.css';
 const Layout = ({ children }: any) => {
   return (
     <div className={styles.global_wrapper}>
+      <div className={styles.registation}>
+        <button className={styles.registation_button}>Регистрация</button>
+      </div>
       <Navbar>{children}</Navbar>
     </div>
   );

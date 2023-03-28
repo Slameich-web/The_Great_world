50a2e3c9c447e510aaa1fff2682cc2fa80abd2d4
Slameich-web <slameacer@mail.ru>
2023-03-28 23:41:24 +0300

    Logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,31 @@
 import Navbar from './navbar';
 import styles from '../styles/Layout.module.css';
+import Link from 'next/link';
 
 const Layout = ({ children }: any) => {
   return (
     <div className={styles.global_wrapper}>
-      <div className={styles.registation}>
-        <button className={styles.registation_button}>
-          <span>Регистрация</span>
-        </button>
-        <button className={styles.download_button}>
-          <span>Скачать игру</span>
-        </button>
+      <div className={styles.global}>
+        <div className={styles.news}>
+          <button className={styles.download_button}>
+            <span>Активности</span>
+          </button>
+        </div>
+        <div className={styles.logo_container}>
+          <Link href="/">
+            <a>
+              <img className={styles.logo} src="https://img.youtube.com/vi/gqN50mBWsH0/maxresdefault.jpg" />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.registation}>
+          <button className={styles.registation_button}>
+            <span>Регистрация</span>
+          </button>
+          <button className={styles.download_button}>
+            <span>Скачать игру</span>
+          </button>
+        </div>
       </div>
       <Navbar>{children}</Navbar>
     </div>

22fb357c5c1fae4b78854c0accacbaa3703765f2
Slameich-web <slameacer@mail.ru>
2023-03-29 23:55:23 +0300

    types

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,13 @@
 import styles from './styles.module.css';
 
-export const Button = ({ variant, text }: any) => {
+type Variant = 'download_button' | 'registation_button';
+
+interface ButtonProps {
+  text: string;
+  variant?: Variant;
+}
+
+export const Button = ({ variant, text }: ButtonProps) => {
   if (variant === 'download_button') {
     return (
       <button className={styles.download_button}>
